d8559edb5ecc7f81f58594373c85bc93501070c4
Mitchell Fagert <mitchell.fagert@gmail.com>
2022-08-24 11:35:42 -0400

    Final Upload.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import scraping
 app = Flask(__name__)
 
 #Use flask_pymongo to set up mongo connection
-app.confi["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
